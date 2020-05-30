67b7868c27383dbc64372e0929431d3d262fb70a
svp62 <svp62@njit.edu>
2020-05-30 15:57:57 -0400

    update update file

--- public_html/update.php
+++ public_html/update.php
@@ -5,13 +5,12 @@ $connection_string = "mysql:host=$dbhost;dbname=$dbdatabase;charset=utf8mb4";
 try{
 	$db = new PDO($connection_string, $dbuser, $dbpass);
 	$stmt = $db->prepare("UPDATE Users set email=:email WHERE email = :original");
-	$stmt->bindValue(":email","newemail.test.com");
-	$stmt->bindVaule(":original","shivani@test.com");
+	$stmt->bindValue(":email","newemail@test.com");
+	$stmt->bindValue(":original","shivani@test.com");
 	$r = $stmt->execute();
 	
 	echo var_export($stmt->errorInfo(), true);
 	echo var_export($r, true);
-	
 }
 catch (Exception $e){
 	echo $e->getMessage();
