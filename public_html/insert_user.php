e09fe82a20650e11d659ad4966c004006e3eb864
svp62 <svp62@njit.edu>
2020-05-30 11:22:07 -0400

    insert changes

--- public_html/insert_user.php
+++ public_html/insert_user.php
@@ -4,11 +4,10 @@ require("config.php");
 $connection_string = "mysql:host=$dbhost;dbname=$dbdatabase;charset=utf8mb4";
 try{
 	$db = new PDO($connection_string, $dbuser, $dbpass);
-	$stmt = $db->prepare("INSERT INTO Users (email) VALUES (:email)";
-	$r = $stmt->execute(array( 
-		":email"=>"shivani@test.com"
+	$stmt = $db->prepare("INSERT INTO Users (email) VALUES (:email)");
+	$r = $stmt->execute(array(
+		":email"=>"test@test.com"
 	));
-	
 	echo var_export($stmt->errorInfo(), true);
 	echo var_export($r, true);
 }
