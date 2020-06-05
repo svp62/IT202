08c841f7f110366b6406dd69170678208f98cb07
svp62 <svp62@njit.edu>
2020-06-05 00:08:37 -0400

    new folder created

---
+++ public_html/sessions/register.php
@@ -0,0 +1,49 @@
+<form method="POST">
+	<label for="email">Email
+	<input type="email" id="email" name="email"/>
+	</label>
+	<label for="p">Password
+	<input type="password" id="p" name="password"/>
+	</label>
+	<label for="cp">Confirm Password
+	<input type="password" id="cp" name="cpassword"/>
+	</label>
+	<input type="submit" name="register" value="Register"/>
+</form>
+
+<?php
+
+if(isset($_POST["register"])){
+	if(isset($_POST["password"]) && isset($_POST["cpassword"]) && isset($_POST["email"])){
+		$password = $_POST["password"];
+		$cpassword = $_POST["cpassword"];
+		$email = $_POST["email"];
+		if($password == $cpassword){
+			require("config.php");
+			$connection_string = "mysql:host=$dbhost;dbname=$dbdatabase;charset=utf8mb4";
+			try{
+				$db = new PDO($connection_string, $dbuser, $dbpass);
+				$hash = password_hash($password, PASSWORD_BCRYPT);
+				$stmt = $db->prepare("INSERT INTO Users (email, password) VALUES(:email, :password)");
+				$stmt->execute(array(
+					":email" => $email,
+					":password" => $hash
+				));
+				$e = $stmt->errorInfo();
+				if($e[0] != "00000"){
+					echo var_export($e, true);
+				}
+				else{
+					echo "<div>REGISTRATION SUCCEED!</div>";
+				}
+			}
+			catch (Exception $e){
+				echo $e->getMessage();
+			}
+		}
+		else{
+			echo "<div>Passwords don't match</div>";
+		}
+	}
+}
+?>
