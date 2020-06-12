ffb09e282141a0b8d38351d14f8db0b6fd6856b8
svp62 <svp62@njit.edu>
2020-06-12 15:05:46 -0400

    changes

--- public_html/milestone1/logout.php
+++ public_html/milestone1/logout.php
@@ -8,6 +8,8 @@ session_unset();
 session_destroy();
 echo "You have been logged out ";
 //echo var_export($_SESSION, true);
+/*
+
 if (ini_get("session.use_cookies")) { 
     $params = session_get_cookie_params(); 
     setcookie(session_name(), '', time() - 42000, 
@@ -16,10 +18,13 @@ if (ini_get("session.use_cookies")) {
     ); 
 }
 
+*/
+
 if(isset($_POST["logout"])) {
  header('Location: '.$_POST["logout"]);  
 } else {
 	echo "you have logged out please login again";
+	
  header('Location: login.php');  
 }
 
