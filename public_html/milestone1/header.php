d68488129c8e3541fc1ab22d07a463761a834bc4
svp62 <svp62@njit.edu>
2020-06-11 23:56:08 -0400

    Update header.php

--- public_html/milestone1/header.php
+++ public_html/milestone1/header.php
@@ -7,10 +7,9 @@ require("config.php");
 session_start();
 ?>
 <nav>
-    <ul>
-        <li><a href="home.php">Home</a></li>
-        <li><a href="login.php">Login</a></li>
-        <li><a href="register.php">Register</a></li>
-        <li><a href="logout.php">Logout</a></li>
-    </ul>
+    <ul><li><a href="home.php">Home</a></li></ul>
+        <ul><li><a href="login.php">Login</a></li></ul>
+        <ul><li><a href="register.php">Register</a></li></ul>
+        <ul><li><a href="logout.php">Logout</a></li></ul>
+    
 </nav>
