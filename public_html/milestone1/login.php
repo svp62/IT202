08666b24a5c8fcb928e836692626cb48767606a8
svp62 <svp62@njit.edu>
2020-06-11 23:42:55 -0400

    style changed

--- public_html/milestone1/login.php
+++ public_html/milestone1/login.php
@@ -4,16 +4,6 @@ include("header.php");
 ?>
 
 <h4>LOGIN</h4>
-<div>
-<nav>
-    <ul>
-        <li><a href="home.php">Home</a></li>
-        <li><a href="login.php">Login</a></li>
-        <li><a href="register.php">Register</a></li>
-        <li><a href="logout.php">Logout</a></li>
-    </ul>
-</nav>
-</div>
 
 
 <form method="POST">
