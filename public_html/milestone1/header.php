e8f11c2ca9cc9c82fcc7db9c3575b1f68585d0b7
svp62 <svp62@njit.edu>
2020-06-12 00:00:33 -0400

    Changes in Header

--- public_html/milestone1/header.php
+++ public_html/milestone1/header.php
@@ -1,15 +1,16 @@
 <head>
     <title>Welcome to the survey page</title>
-    
+    <link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <?php
 require("config.php");
 session_start();
 ?>
 <nav>
-    <ul><li><a href="home.php">Home</a></li></ul>
-        <ul><li><a href="login.php">Login</a></li></ul>
-        <ul><li><a href="register.php">Register</a></li></ul>
-        <ul><li><a href="logout.php">Logout</a></li></ul>
-    
+    <ul>
+        <li><a href="home.php">Home</a></li>
+        <li><a href="login.php">Login</a></li>
+        <li><a href="register.php">Register</a></li>
+        <li><a href="logout.php">Logout</a></li>
+    </ul>
 </nav>
