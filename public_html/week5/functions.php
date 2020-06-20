910d6fa9ad1a423a36f6be167ad4e75203141fd7
svp62 <svp62@njit.edu>
2020-06-20 18:37:25 -0400

    ready to deliver

--- public_html/week5/functions.php
+++ public_html/week5/functions.php
@@ -15,23 +15,6 @@ function getDB(){
     return $db;
 }
 
-function getID(){
-    //global $db;
-	global $idnum = -1;
-    if(isset($_GET["idnum"])){
-		
-		$idnum = $_GET["idnum"];
-		$stmt = $db->prepare("SELECT * FROM Survey where id = :id");
-		$stmt->execute([":id"=>$idnum]);
-		$result = $stmt->fetch(PDO::FETCH_ASSOC);
-	
-	}
-	
-	else{
-		
-		echo "ID not provided in url. Please put '?id=("id number where you want to update data")' at the end of URL. ";
-		
-	}
-}
+
 
 ?>
