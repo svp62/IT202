372a6ee9b3e71e152cbdc025fd7cececc670abd2
svp62 <svp62@njit.edu>
2020-06-20 16:46:32 -0400

    typo fixed

--- public_html/week5/create.php
+++ public_html/week5/create.php
@@ -17,11 +17,11 @@ if(isset($_POST["created"])){
     $visibility = $_POST["visibility"];
     if(!empty($title) && !empty($description) && !empty($visibility)){
         //require("config.php");
-		require("common.inc.php");
-		$db = getDB();
+		require("functions.php");
+		//$db = getDB();
         //$connection_string = "mysql:host=$dbhost;dbname=$dbdatabase;charset=utf8mb4";
         try{
-            //$db = getDB();
+            $db = getDB();
             $stmt = $db->prepare("INSERT INTO Survey (title, description, visibility) VALUES (:title, :description, :visibility)");
             $result = $stmt->execute(array(
                 ":title" => $title,
