b4cae8f131a9dd7b99450c137fa3b905250668b3
svp62 <svp62@njit.edu>
2020-06-20 15:08:41 -0400

    update

--- public_html/week5/test.php
+++ public_html/week5/test.php
@@ -5,10 +5,11 @@ $connection_string = "mysql:host=$dbhost;dbname=$dbdatabase;charset=utf8mb4";
 try{
 	$db = new PDO($connection_string, $dbuser, $dbpass);
 	$stmt = $db->prepare("CREATE TABLE `Survey` (
-				`id` int auto_increment,
+				`id` int auto_increment not null,
 				`title` varchar(30) not null unique,
 				`description` TEXT,
 				`visibility` int,
+				`created` timestamp default current_timestamp,
 				PRIMARY KEY (`id`)
 				) CHARACTER SET utf8 COLLATE utf8_general_ci");
 	$r = $stmt->execute();
