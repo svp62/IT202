cb7266d0a125a637ada386e007d2d6c3675d6afd
svp62 <svp62@njit.edu>
2020-06-20 14:43:07 -0400

    new folder

---
+++ public_html/Week5_homework/creating.php
@@ -0,0 +1,21 @@
+<?php
+require("config.php");
+
+$connection_string = "mysql:host=$dbhost;dbname=$dbdatabase;charset=utf8mb4";
+try{
+	$db = new PDO($connection_string, $dbuser, $dbpass);
+	$stmt = $db->prepare("CREATE TABLE `Survey` (
+				`id` int auto_increment,
+				`title` varchar(30) not null unique,
+				`description` TEXT,
+				`visibility` int,
+				PRIMARY KEY (`id`)
+				) CHARACTER SET utf8 COLLATE utf8_general_ci");
+	$r = $stmt->execute();
+	echo var_export($stmt->errorInfo(), true);
+	echo var_export($r, true);
+}
+catch (Exception $e){
+	echo $e->getMessage();
+}
+?>
