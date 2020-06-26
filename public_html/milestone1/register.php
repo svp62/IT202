402952eab4f66569200ae98c76f97926d974d180
svp62 <svp62@njit.edu>
2020-06-25 23:09:08 -0400

    Update register.php

--- public_html/milestone1/register.php
+++ public_html/milestone1/register.php
@@ -2,31 +2,43 @@
 include("header.php");
 
 ?>
-<h1>  </h1>
-<h2>Please Register here. </h2>
 
-
-<form method="POST">
-	<label for="email">Email
-	<input type="email" id="email" name="email" required />
-	</label><br><br>
-	<label for="p">Password
-	<input type="password" id="p" name="password" required />
-	</label><br><br>
-	<label for="cp">Confirm Password
-	<input type="password" id="cp" name="cpassword" required />
-	</label><br><br><br>
-	<input type="submit" name="register" value="Register"/>
-</form>
 
 
 <?php
 
+$emailerr = $passerr = "";
+$email = $password = "";
+
 if(isset($_POST["register"])){
-	if(isset($_POST["password"]) && isset($_POST["cpassword"]) && isset($_POST["email"])){
-		$password = $_POST["password"];
-		$cpassword = $_POST["cpassword"];
-		$email = $_POST["email"];
+	
+	if(empty($_POST["email"])){
+			
+			$emailerr = "Email required";
+			
+			
+		} else {
+			$email = $_POST["email"];
+		}
+		if(empty($_POST["password"])){
+			
+			$passerr = "Password required";
+		} else {
+			$password = $_POST["password"];
+			
+		}
+		if(empty($_POST["cpassword"])){
+			
+			$passerr1 = "Password confirmation required";
+		} else {
+			$cpassword = $_POST["cpassword"];
+			
+		}
+		
+	if(!empty($email) && !empty($password) && !empty($_POST["cpassword"])){
+		//$password = $_POST["password"];
+		//$cpassword = $_POST["cpassword"];
+		//$email = $_POST["email"];
 		if($password == $cpassword){
 			//require("config.php");
 			$connection_string = "mysql:host=$dbhost;dbname=$dbdatabase;charset=utf8mb4";
@@ -56,3 +68,28 @@ if(isset($_POST["register"])){
 	}
 }
 ?>
+
+
+<h1>  </h1>
+<h2>Please Register here. </h2>
+
+<span class="error">* required field</span>
+<form method="POST">
+
+	<label for="email">Email
+	<input type="email" id="email" name="email" />
+	<span class="error" id="email">* <?php echo $emailerr;?></span>
+	</label><br><br>
+	
+	<label for="p">Password
+	<input type="password" id="p" name="password" />
+	<span class="error" id="p">* <?php echo $passerr;?></span>
+	</label><br><br><br>
+	
+	<label for="cp">Confirm Password
+	<input type="password" id="cp" name="cpassword" required />
+	<span class="error" id="cp">* <?php echo $passerr1;?></span>
+	</label><br><br><br>
+	
+	<input type="submit" name="register" value="Register"/>
+</form>
