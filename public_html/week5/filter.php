dcf238ca2dbe38bfc487eef711463da4b78c38b7
svp62 <svp62@njit.edu>
2020-06-28 14:54:35 -0400

    styling

--- public_html/week5/filter.php
+++ public_html/week5/filter.php
@@ -1,3 +1,11 @@
+
+<?php
+include("styles.php");
+
+?>
+
+
+
 <?php
 $filter = "";
 if(isset($_POST["filter"])){
@@ -5,22 +13,24 @@ if(isset($_POST["filter"])){
 }
 ?>
 <form method="POST">
+	<div>
     <input type="text" name="filter" placeholder="Filter for Title"
     value="<?php echo $filter;?>"/>
     <input type="submit" value="Filter"/>
+	</div>
 </form>
 <?php
 if(isset($filter)) {
 
     require("functions.php");
-    //$query = file_get_contents(__DIR__ . "/queries/SEARCH_TABLE_THINGS.sql");
+    
 	$query = file_get_contents("filter_table.sql");
     if (isset($query) && !empty($query)) {
         try {
             $stmt = getDB()->prepare($query);
-            //Note: With a LIKE query, we must pass the % during the mapping
+            
             $stmt->execute([":filter"=>$filter]);
-            //Note the fetchAll(), we need to use it over fetch() if we expect >1 record
+            
             $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
         } catch (Exception $e) {
             echo $e->getMessage();
@@ -28,22 +38,20 @@ if(isset($filter)) {
     }
 }
 ?>
-<!--This part will introduce us to PHP templating,
-note the structure and the ":" -->
-<!-- note how we must close each check we're doing as well-->
+
+
 <?php if(isset($results) && count($results) > 0):?>
-    <p>This shows when we have results</p>
+    <p>we have results below.</p>
     <ul>
-        <!-- Here we'll loop over all our results and reuse a specific template for each iteration,
-        we're also using our helper function to safely return a value based on our key/column name.-->
+        
         <?php foreach($results as $row):?>
             <li>
                 <?php echo get($row, "title")?>
                 <?php echo get($row, "description");?>
-                <a href="delete.php?thingId=<?php echo get($row, "id");?>">Delete</a>
+                <!--<a href="delete.php?thingId=<?php echo get($row, "id");?>">Delete</a> -->
             </li>
         <?php endforeach;?>
     </ul>
 <?php else:?>
-    <p>This shows when we don't have results</p>
+    <p>we don't have any results.</p>
 <?php endif;?>
