e6db6dc5e2ea18f6e2b59952ab9a0ff309b3957f
svp62 <svp62@njit.edu>
2020-07-07 14:01:33 -0400

    visuals changed

--- public_html/milestone2/create.php
+++ public_html/milestone2/create.php
@@ -34,7 +34,9 @@ if(isset($_POST["created"])){
 	if(empty($_POST["question1"]) && empty($_POST["question2"]) && empty($_POST["question3"])){
 			
 			$queserr = "Question needed";
+			echo"-------------------------------------------------";
 			echo "YOU ARE REQUIRED TO ADD 3 QUESTION TO SURVEY.";
+			echo"-------------------------------------------------";
 		}
 		else{
 			$question1 = $_POST["question1"];
