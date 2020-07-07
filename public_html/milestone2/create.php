8a27aa9186f95c296df396c42bc99383723ca955
svp62 <svp62@njit.edu>
2020-07-07 13:59:07 -0400

    updated create

--- public_html/milestone2/create.php
+++ public_html/milestone2/create.php
@@ -5,60 +5,41 @@ include("header.php");
 
 
 
-<div>
-<form method="POST">
-    <label for="title">Title
-	<input type="text" id="title" name="title" />
-	</label><br><br>
-	<label for="description">Description
-	<input type="text" id="description" name="description" />
-	</label><br><br>
-	<label for="question1">Question 1
-	<input type="text" id="question1" name="question1" />
-	</label><br><br>
-	<label for="question2">Question 2
-	<input type="text" id="question2" name="question2" />
-	</label><br><br>
-	<label for="question3">Question 3
-	<input type="text" id="question3" name="question3" />
-	</label><br><br>
-	<label for="question4">Question 4
-	<input type="text" id="question4" name="question4" />
-	</label><br><br>
-	<label for="question5">Question 5
-	<input type="text" id="question5" name="question5" />
-	</label><br><br>
-	<label for="visibility">Visibility
-	<input type="number" id="visibility" name="visibility" />
-	</label><br><br>
-    <input type="submit" name="created" value="Create Survey"/>
-</form>
-</div>
+
 <?php
 if(isset($_POST["created"])){
-    $title = $_POST["title"];
-    $description = $_POST["description"];
+    
+    
     $visibility = $_POST["visibility"];
-	$question1 = $_POST["question1"];
-	$question2 = $_POST["question2"];
-	$question3 = $_POST["question3"];
 	$question4 = $_POST["question4"];
 	$question5 = $_POST["question5"];
 	
-	if(empty($title)){
+	if(empty($_POST["title"])){
 			
-			echo "Title needed";
-			
-		}
-	if(empty($description)){
-			
-			echo "Description needed";
+			$titleerr = "Title needed";
 			
 		}
-	if(empty($question1) && empty($question2) && empty($question3)){
+		else{
+			$title = $_POST["title"];
+		}
+	if(empty($_POST["description"])){
 			
-			echo "3 questions needed";
+			$deserr = "Description needed";
 			
+		}
+		else{
+			$description = $_POST["description"];
+		}
+	
+	if(empty($_POST["question1"]) && empty($_POST["question2"]) && empty($_POST["question3"])){
+			
+			$queserr = "Question needed";
+			echo "YOU ARE REQUIRED TO ADD 3 QUESTION TO SURVEY.";
+		}
+		else{
+			$question1 = $_POST["question1"];
+			$question2 = $_POST["question2"];
+			$question3 = $_POST["question3"];
 		}
 	
 	
@@ -100,8 +81,42 @@ if(isset($_POST["created"])){
             echo $e->getMessage();
         }
     }
-    else{
-        echo "Title, Description and Visibility fields cannot be empty.";
-    }
+    
 }
 ?>
+
+
+<div>
+<form method="POST">
+    <label for="title">Title
+	<input type="text" id="title" name="title" />
+	<span class="error" id="title">* <?php echo $titleerr;?></span>
+	</label><br><br>
+	<label for="description">Description
+	<input type="text" id="description" name="description" />
+	<span class="error" id="description">* <?php echo $deserr;?></span>
+	</label><br><br>
+	<label for="question1">Question 1
+	<input type="text" id="question1" name="question1" />
+	<span class="error" id="question1">* <?php echo $queserr;?></span>
+	</label><br><br>
+	<label for="question2">Question 2
+	<input type="text" id="question2" name="question2" />
+	<span class="error" id="question2">* <?php echo $queserr;?></span>
+	</label><br><br>
+	<label for="question3">Question 3
+	<input type="text" id="question3" name="question3" />
+	<span class="error" id="question2">* <?php echo $queserr;?></span>
+	</label><br><br>
+	<label for="question4">Question 4
+	<input type="text" id="question4" name="question4" />
+	</label><br><br>
+	<label for="question5">Question 5
+	<input type="text" id="question5" name="question5" />
+	</label><br><br>
+	<label for="visibility">Visibility
+	<input type="number" id="visibility" name="visibility" />
+	</label><br><br>
+    <input type="submit" name="created" value="Create Survey"/>
+</form>
+</div>
