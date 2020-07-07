645293b2f3d5aef1b9eb87ae76931f238b2ba773
svp62 <svp62@njit.edu>
2020-07-07 10:54:56 -0400

    images added

--- public_html/milestone2/home.php
+++ public_html/milestone2/home.php
@@ -1,7 +1,23 @@
+
+
+
 <?php
 include("header.php");
 
 ?>
+
+<style>
+
+body {
+	background-color: lightgrey;
+	background-image: homep.jpg;
+	text-align: center;
+	font-size: 32px
+	font-family: "Times New Roman", Times, serif;
+	
+}
+
+</style>
 
 <h1>     </h1>
 
@@ -9,4 +25,23 @@ include("header.php");
 <?php
 //session_start();
 echo "Welcome to the survey homepage, " . $_SESSION["user"]["email"];
+
 ?>
+<h1>  </h1>
+<h2>Please Login with your existing account or register to start taking surveys.</h2>
+
+<div>
+<span class="error">* required field</span>
+<form method="POST">
+	<label for="email">Email
+	<input type="email" id="email" name="email" />
+	<span class="error" id="email">* <?php echo $emailerr;?></span>
+	</label><br><br>
+	
+	<label for="p">Password
+	<input type="password" id="p" name="password" />
+	<span class="error" id="p">* <?php echo $passerr;?></span>
+	</label><br><br><br>
+	<input type="submit" name="login" value="Login"/>
+</form>
+</div>
