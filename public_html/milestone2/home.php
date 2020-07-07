fbf5293b7346a9ef55fbb5a0b916447588e026cb
svp62 <svp62@njit.edu>
2020-07-07 11:02:43 -0400

    Update home.php

--- public_html/milestone2/home.php
+++ public_html/milestone2/home.php
@@ -13,31 +13,21 @@ include("header.php");
 
 <?php
 //session_start();
-echo "Welcome to the survey homepage, " . $_SESSION["user"]["email"];
+echo "Welcome to the survey page, " . $_SESSION["user"]["email"];
+//width="600" height="400"
 
 ?>
 <h1>  </h1>
-<h2>Please Login with your existing account or register to start taking surveys.</h2>
+<h2>  </h2>
 
-<div>
-
-
-    <a target="_blank" href="homep.jpg">
-      <img src="homep.jpg" width="600" height="400">
+<a target="_blank" href="homep.jpg">
+      <img src="homep.jpg" >
     </a>
+
     
 
-<span class="error">* required field</span>
+
 <form method="POST">
-	<label for="email">Email
-	<input type="email" id="email" name="email" />
-	<span class="error" id="email">* <?php echo $emailerr;?></span>
-	</label><br><br>
 	
-	<label for="p">Password
-	<input type="password" id="p" name="password" />
-	<span class="error" id="p">* <?php echo $passerr;?></span>
-	</label><br><br><br>
-	<input type="submit" name="login" value="Login"/>
+	<input type="submit" name="login" value="GET STARTED"/>
 </form>
-</div>
