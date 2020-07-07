06c1ac5603df3c6eba10f76783587713be8ac274
svp62 <svp62@njit.edu>
2020-07-07 18:15:39 -0400

    Update created.php

---
+++ public_html/milestone2/created.php
@@ -0,0 +1,23 @@
+<?php
+include("header.php");
+
+?>
+<?php
+if(isset($_POST["create"])){
+	
+	$connection_string = "mysql:host=$dbhost;dbname=$dbdatabase;charset=utf8mb4";
+        try{
+           
+			$db = new PDO($connection_string, $dbuser, $dbpass);
+			$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $stmt = "DROP TABLE Survey";
+			$db->exec($stmt);
+		echo"Record deleted successfully";
+            
+        }
+        catch (PDOException $e){
+            echo " error ayi " . $e->getMessage();
+        }
+	
+	
+}
