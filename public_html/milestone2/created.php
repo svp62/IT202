47a277772e6137cfcefb3c07c01707189b4d4fb9
svp62 <svp62@njit.edu>
2020-07-08 01:45:36 -0400

    Update created.php

--- public_html/milestone2/created.php
+++ public_html/milestone2/created.php
@@ -72,10 +72,71 @@ catch (Exception $e){
 						<?php echo "<br> QUESTION 5: " . $row["question5"]; ?>
 						<input type="text" id="ans5" name="ans5"><br>
 						<button class="button3" name="click" >DONE</button>
-						<?php } ?>
+						
+						<?php
+							if(isset($_POST["click"])){
+
+										$connection_string = "mysql:host=$dbhost;dbname=$dbdatabase;charset=utf8mb4";
+										$ans1 = $_POST["ans1"];
+										$ans2 = $_POST["ans2"];
+										$ans3 = $_POST["ans3"];
+										$ans4 = $_POST["ans4"];
+										$ans5 = $_POST["ans5"];
+										
+										echo $ans1;
+										echo $ans2;
+										echo $ans3;
+										echo $ans4;
+										echo $ans5;
+										
+									try{
+									   
+										$db = new PDO($connection_string, $dbuser, $dbpass);
+										$stmt = $db->prepare("INSERT INTO Survey_questions (ques1, ques2, ques3, ques4, ques5, ans1, ans2, ans3, ans4, ans5) VALUES (:ques1, :ques2, :ques3, :ques4, :ques5, :ans1, :ans2, :ans3, :ans4, :ans5)");
+										$result = $stmt->execute(array(
+											":ques1" => $row["question1"],
+											":ques2" => $row["question2"],
+											":ques3" => $row["question3"],
+											":ques4" => $row["question4"],
+											":ques5" => $row["question5"],
+											":ans1" => $ans1,
+											":ans2" => $ans2,
+											":ans3" => $ans3,
+											":ans4" => $ans4,
+											":ans5" => $ans5
+											
+										));
+										
+										$e = $stmt->errorInfo();
+										if($e[0] != "00000"){
+											echo var_export($e, true);
+										}
+										else{
+											
+											if ($result){
+												echo"<br>---------------------------------------------------------------------------------<br>";
+												echo "Successfully recorded your survey ";
+												echo"<br>---------------------------------------------------------------------------------<br>";
+											}
+											else{
+												echo "Error creating data";
+											}
+											} 
+									}
+
+							catch (Exception $e){
+										echo $e->getMessage();
+									}
+
+											 }
+
+						?>	
+						
+					<?php } ?>
 						
 						
 <?php
+/*
 if(isset($_POST["click"])){
 
 			$connection_string = "mysql:host=$dbhost;dbname=$dbdatabase;charset=utf8mb4";
@@ -131,7 +192,7 @@ catch (Exception $e){
         }
 
 				 }
-
+*/
 ?>	
                 
             
