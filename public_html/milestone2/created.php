9313afa5dfdc1c2c7f4502597de9f5cbc60f18cb
svp62 <svp62@njit.edu>
2020-07-08 00:22:51 -0400

    files changed

--- public_html/milestone2/created.php
+++ public_html/milestone2/created.php
@@ -5,7 +5,9 @@ include("header.php");
 <div>
 <form method="POST">
     
-    <input type="submit" name="create" value="SHOW ME OUTPUT"/>
+    <input type="radio" name="sephora" value="SEPHORA"/>
+	<input type="radio" name="tony" value="TONY'S COSMETICS"/>
+	<input type="submit" value="Submit">
 </form>
 </div>
 <?php
@@ -18,16 +20,10 @@ if(isset($_POST["create"])){
 			$db = new PDO($connection_string, $dbuser, $dbpass);
 			$sql = "SELECT * FROM Survey";
             $stmt = $db->query($sql);
-			//$stmt->execute();
-			//$result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
-
-			 //$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+			
 			 
             $e = $stmt->errorInfo();
-            
-                
-                //if ($result > 0){
-					// output data of each row
+				
 					foreach ($stmt as $row) {
     
 
@@ -35,17 +31,14 @@ if(isset($_POST["create"])){
 						echo "<br> Title: " . $row["title"];
 						echo "<br> DESCRIPTION: " . $row["description"];
 						echo "<br> QUESTION 1: " . $row["question1"];
-						echo "<br> DQUESTION 2: " . $row["question2"];
+						echo "<br> DQUESTION 2: " . $row["question2"]; 
 						echo "<br> QUESTION 3: " . $row["question3"];
 						echo "<br> QUESTION 4: " . $row["question4"];
 						echo "<br> QUESTION 5: " . $row["question5"];
 						echo "<br> VISIBILITY: " . $row["visibility"];
 						
 						}
-                //}
-                //else{
-                   // echo "0 results to display";
-                //}
+                
             
         }
         catch (Exception $e){
@@ -56,3 +49,42 @@ if(isset($_POST["create"])){
 }
 
 ?>
+
+
+<?php if(isset($_POST["sephora"])) {   $fetchqry = "SELECT * FROM `Survey` where id=1"; 
+
+
+			$connection_string = "mysql:host=$dbhost;dbname=$dbdatabase;charset=utf8mb4";
+        try{
+           
+			
+			$db = new PDO($connection_string, $dbuser, $dbpass);
+			$sql = "SELECT * FROM `Survey` where id=1";
+            $stmt = $db->query($sql);
+			
+			 
+            $e = $stmt->errorInfo();
+
+					foreach ($stmt as $row) {
+    
+						echo "<br> ID: " . $row["id"]; 
+						echo "<br> Title: " . $row["title"];
+						echo "<br> DESCRIPTION: " . $row["description"];
+						echo "<br> QUESTION 1: " . $row["question1"]; 
+						echo "<br> DQUESTION 2: " . $row["question2"];
+						echo "<br> QUESTION 3: " . $row["question3"];
+						echo "<br> QUESTION 4: " . $row["question4"];
+						echo "<br> QUESTION 5: " . $row["question5"];
+						echo "<br> VISIBILITY: " . $row["visibility"];
+						 
+						
+						}
+                
+            
+        }
+        catch (Exception $e){
+            echo $e->getMessage();
+        }
+
+				 }
+		  ?>
