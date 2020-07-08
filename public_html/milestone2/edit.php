c68198259295f6b4be65e9f8cfead4b068f21984
svp62 <svp62@njit.edu>
2020-07-08 02:47:24 -0400

    Update edit.php

--- public_html/milestone2/edit.php
+++ public_html/milestone2/edit.php
@@ -4,8 +4,8 @@ include("header.php");
 ?>
 
 <?php
-require("functions.php");
-$db = getDB();
+$connection_string = "mysql:host=$dbhost;dbname=$dbdatabase;charset=utf8mb4";
+ $db = new PDO($connection_string, $dbuser, $dbpass);
 $idnum = -1;
 $result = array();
 
