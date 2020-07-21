bda660458e221efef63520b60b608e21ced1972f
svp62 <svp62@njit.edu>
2020-07-21 17:38:18 -0400

    Update created.php

--- public_html/milestone2/created.php
+++ public_html/milestone2/created.php
@@ -71,7 +71,7 @@ catch (Exception $e){
 						<input type="text" id="ans4" name="ans4"><br>
 						<?php echo "<br> QUESTION 5: " . $row["question5"]; ?>
 						<input  type="text" id="ans5" name="ans5"><br>
-						<button class="button3" name="click" >DONE</button>
+						<input class="button3" name="click" >DONE</input>
 						
 						<?php } ?>
 						
