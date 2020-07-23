f2c980d9afdfd7d4aed3a7015005d38a25c8912a
svp62 <svp62@njit.edu>
2020-07-23 16:54:33 -0400

    Update survey.php

--- public_html/project/survey.php
+++ public_html/project/survey.php
@@ -23,7 +23,7 @@ include("header.php");
 		if ( $stmt = $db->query($sql)) {
 			while ($row = $stmt->fetch(PDO::FETCH_NUM)) {
 				$term = $row[0];
-				$sendarray[] = array("examname" => $term);
+				$sendarray[] = array("name" => $term);
 			}
 			}
 		 $e = $stmt->errorInfo();
@@ -33,8 +33,8 @@ include("header.php");
             echo $e->getMessage();
         }
 
-$finalarray = json_encode($sendarray);
+//$finalarray = json_encode($sendarray);
 
-echo $finalarray;
+echo $sendarray;
 
 ?> 
