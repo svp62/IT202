836d6aabe22a8b8c9b66411af0f1c07ab2b56f5f
svp62 <svp62@njit.edu>
2020-07-23 18:37:18 -0400

    Update questions.php

--- public_html/project/questions.php
+++ public_html/project/questions.php
@@ -11,7 +11,7 @@
            	
 			$db = new PDO($connection_string, $dbuser, $dbpass);
 		
-            $sql = "SELECT id FROM Survey WHERE id=$id";
+            $sql = "SELECT id FROM Survey WHERE id='$id'";
 			
 		if ( $stmt = $db->query($sql)) {
 			$row = $stmt->fetch(PDO::FETCH_NUM);
@@ -27,7 +27,7 @@
            	
 			$db = new PDO($connection_string, $dbuser, $dbpass);
 		
-            $sql = "SELECT question1, question2, question3, question4, question5 FROM Questions WHERE id=$id";
+            $sql = "SELECT question1, question2, question3, question4, question5 FROM Questions WHERE id='$id'";
 			
 		if ( $stmt = $db->query($sql)){
 			$row = $stmt->fetch(PDO::FETCH_NUM);
