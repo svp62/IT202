c354a55f21500e01f8f5092f8acec3faaa8ac42a
svp62 <svp62@njit.edu>
2020-07-25 19:40:27 -0400

    fixed

--- public_html/surveyfinal/include/db_connection.php
+++ public_html/surveyfinal/include/db_connection.php
@@ -5,6 +5,21 @@ $dbuser = $cleardb_url["user"];
 $dbpass = $cleardb_url["pass"];
 $dbdatabase = substr($cleardb_url["path"],1);
 
+
+try{
+		
+		$db = "mysql:host=$dbhost;dbname=$dbdatabase;charset=utf8mb4";
+		
+		//$conn=new PDO("mysql:host=$host;dbname=$dbName",$dbuser,$dbpass);
+		
+		$conn = new PDO($db, $dbuser, $dbpass);
+		
+
+	}catch(PDOException $e)
+	{
+		echo $e->getMessage(); 
+	}
+
 ?>
 
 
